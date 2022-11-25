65476e05e59098b7907e1f0db4155240272d1548
DeNat1ve <deniskutt@yadnex.ru>
2022-11-25 12:04:02 +0300

    add test interaction with API

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import Header from "./components/header/Header.jsx";
 import BlockList from "./components/blockList/BlockList.jsx";
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 function App() {
   const [blocks, setBlocks] = useState([
-    { id: 1, title: 'ReactExample1', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste vitae assumenda, minus perferendis amet error cum similique libero quis recusandae, adipisci eum itaque nemo aspernatur nobis quo. Illum, eius ipsum unde omnis eos mollitia in! Quidem excepturi optio quos!' },
-    { id: 2, title: 'ReactExample2', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste vitae assumenda, minus perferendis amet error cum similique libero quis recusandae!' },
-    { id: 3, title: 'ReactExample3', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste vitae assumenda, minus perferendis amet error cum similique libero quis recusandae, adipisci eum itaque nemo aspernatur nobis quo. Illum, eius ipsum unde omnis eos mollitia in!' }
+    { id: 4, title: 'title', body: 'body' }
   ])
 
   const [rating, setRating] = useState([
     { id: 1, likes: 23, dislikes: 4 },
-    { id: 2, likes: 12, dislikes: 2 },
-    { id: 3, likes: 30, dislikes: 8 }
+    { id: 2, likes: 23, dislikes: 4 },
+    { id: 3, likes: 23, dislikes: 4 },
+    { id: 4, likes: 23, dislikes: 4 }
   ])
 
   const removeBlock = ((block) =>
@@ -25,6 +24,21 @@ function App() {
   (setBlocks([...blocks, block]),
     setRating([...rating, { id: block.id, likes: 0, dislikes: 0 }]))
   )
+
+  async function fetchBlocks() {
+    const response = await fetch('https://localhost:44396/block')
+    if (response.ok) {
+      let getBlocks = await response.json()
+      setBlocks([...blocks, ...getBlocks])
+    }
+    else {
+      console.log(response.statusText)
+    }
+  }
+
+  useEffect(() => {
+    fetchBlocks()
+  }, [])
 
   return (
     <div className="App">
